de570f2ed9c1fdfc57ebb868fc502adcb1ea9477
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-07-31 10:39:35 -0400

    Add cron job to update database every minute

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const co = require('co');
+const Cron = require('cron');
 const svnDb = require('./svnDb');
 const database = require('./database');
 const server = require('./server');
@@ -8,17 +9,30 @@ process.on('uncaughtException', (err) => {
     console.error(err);
 });
 
+const initCronJobs = function () {
+    const cacheJob = new Cron.CronJob({
+        cronTime: '* * * * *',
+        onTick: function () {
+            svnDb.update();
+        },
+        start: false
+    });
+
+    cacheJob.start();
+};
+
 co(function* main() {
     // Wait for database to connect.
     yield database.connect();
-
-    // Build / update SVN database.
-    svnDb.update();
 
     // Run server.
     const port = process.env.PORT || 5017;
     server.listen(port);
     console.log(`Server listening on port ${port} ...`);
+}).then(() => {
+    // Build / update SVN database.
+    console.log('Build / update SVN database ...');
+    initCronJobs();
 }).catch((err) => {
     console.error(err);
 });
