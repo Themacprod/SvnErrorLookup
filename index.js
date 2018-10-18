c3c5119b08c42980214da5135d635eac1681f8f4
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-10-18 17:14:46 -0400

    Added cron job to scrap SVN database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const co = require('co');
+const Cron = require('cron');
 const svnDb = require('./svnDb');
 const database = require('./database');
 const server = require('./server');
@@ -7,6 +8,18 @@ const server = require('./server');
 process.on('uncaughtException', (err) => {
     console.error(err);
 });
+
+const initCronJobs = function () {
+    const cacheJob = new Cron.CronJob({
+        cronTime: '* * * * *',
+        onTick: function () {
+            svnDb.update();
+        },
+        start: false
+    });
+
+    cacheJob.start();
+};
 
 co(function* main() {
     // Check environement variable.
@@ -42,13 +55,13 @@ co(function* main() {
     yield database.connect();
 
     // Run server.
-    const port = 5001;
+    const port = 5000;
     server.listen(port);
     console.log(`Server listening on port ${port} ...`);
 }).then(() => {
     // Build / update SVN database.
     console.log('Check / update SVN database ...');
-    svnDb.update();
+    initCronJobs();
 }).catch((err) => {
     console.error(err);
 });
